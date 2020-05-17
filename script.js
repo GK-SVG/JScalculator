3703c68d28a1ec86684581c9a5c2cf00693ae224
gk-svg <gk32239@gmail.com>
2020-05-17 15:30:21 +0530

    logics added to operators and numbers

--- script.js
+++ script.js
@@ -1,13 +1,80 @@
-function getHistory(){
+function getHistory() {
     return document.getElementById('hv').innerText;
 }
-function printHistory(num){
-    document.getElementById('hv').innerText=num;
+
+function printHistory(num) {
+    document.getElementById('hv').innerText = num;
 }
-function getOutput(){
+
+function getOutput() {
     return document.getElementById('opv').innerText;
-} 
-function printOutput(num){
-    document.getElementById('opv').innerText=num;
 }
-printOutput('9878')
+
+function printOutput(num) {
+    if (num == "") {
+        document.getElementById('opv').innerText = num;
+    } else {
+        document.getElementById('opv').innerText = getFormattedNumber(num);
+    }
+}
+
+function getFormattedNumber(num) {
+    if (num == "-") {
+        return "";
+    }
+    var n = Number(num);
+    var value = n.toLocaleString('en');
+    return value
+}
+
+function reverseNumberFormat(num) {
+    return Number(num.replace(/,/g, ''));
+}
+
+var operator = document.getElementsByClassName('operator');
+for (let index = 0; index < operator.length; index++) {
+    operator[index].addEventListener('click', function () {
+        if (this.id == "clear") {
+            printOutput("");
+            printHistory("");
+        } else
+        if (this.id == "back") {
+            var output = reverseNumberFormat(getOutput()).toString();
+            if (output) {
+                output = output.substr(0, output.length - 1);
+                printOutput(output);
+            }
+        } else {
+            var output = getOutput();
+            var history = getHistory();
+            if (output == "" && history != "") {
+                if (isNaN(history[history.length - 1])) {
+                    history = history.substr(0, history.length - 1);
+                }
+            }
+            if (output != "" || history != "") {
+                output = output == "" ? output : reverseNumberFormat(output);
+                history = history + output;
+                if (this.id == "=") {
+                    var result = eval(history);
+                    printOutput(result);
+                    printHistory("");
+                } else {
+                    history = history + this.id;
+                    printHistory(history);
+                    printOutput("");
+                }
+            }
+        }
+    })
+}
+var number = document.getElementsByClassName('number');
+for (let index = 0; index < number.length; index++) {
+    number[index].addEventListener('click', function () {
+        var output = reverseNumberFormat(getOutput());
+        if (output != NaN) { //output is a number
+            output = output + this.id;
+            printOutput(output);
+        }
+    })
+}
